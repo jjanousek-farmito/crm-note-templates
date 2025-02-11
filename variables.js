0fbd6d2af886c86f28253fe1f26a936ae0143b8f
Jáchym Janoušek <janousekjachym@gmail.com>
2025-02-11 21:52:57 +0100

    fix: united variables description

--- variables.js
+++ variables.js
@@ -2,11 +2,11 @@
 // name, variableName, description
 
 export const variables = [
-    ["navyseni", "increasePercentage", "Navýšení ceny v %"],
-    ["cena", "priceWithoutTax", "Cena"],
-    ["cena_dph", "priceWithTax", "Cena s DPH"],
-    ["cena_metr", "pricePerSquareMeter", "Cena za metr"],
-    ["cena_metr_dph", "pricePerSquareMeterWithTax", "Cena za metr s DPH"],
-    ["reinzerce_index", "reAdvertIndex", "Reinzerce index (0-100)"],
+    ["navyseni", "increasePercentage", "Navýšení ceny (00 %)"],
+    ["cena", "priceWithoutTax", "Cena (123 456 Kč)"],
+    ["cena_dph", "priceWithTax", "Cena s DPH (123 456 Kč)"],
+    ["cena_metr", "pricePerSquareMeter", "Cena za metr (123 Kč/m2)"],
+    ["cena_metr_dph", "pricePerSquareMeterWithTax", "Cena za metr s DPH (123 Kč/m2)"],
+    ["reinzerce_index", "reAdvertIndex", "Reinzerce index (0)"],
     ["vymera", "area-total", "Výměra pozemku (123 m2)"],
 ];
